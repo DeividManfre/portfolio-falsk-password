2b8f6dd904147238bb272bbba8fdd68a4ef66a18
Manfre <deividmanfre@gmail.com>
2021-06-09 13:58:25 -0300

    adjust the archive genereitor

--- app.py
+++ app.py
@@ -1,16 +1,5 @@
 from flask import Flask, render_template, request, redirect, session, flash, url_for
-import random
-
-class Genereitor:
-    def genereitor_password(letter, number, number_caracter):
-        try:
-            join_arr = str(letter + "," + number)
-            tratament = join_arr.split(',')
-            randon = random.choices(tratament, k=number_caracter)
-            return "".join(randon)
-        except Error as e:
-            print(e)
-            return e
+from utils.genereitor import Genereitor
 
 app=Flask(__name__)
 
@@ -25,13 +14,7 @@ def genereitor():
     number = request. form['number']
     number_caracter = request. form['number_caracter']
     result = Genereitor.genereitor_password(latter, str(number), int(number_caracter))
-    return redirect(url_for('result'))
-
-@app.route("/result")
-def result():
-    return render_template('result.html')
-
-
+    return result
 
 
 if __name__ == '__main__':
--- utils/genereitor.py
+++ utils/genereitor.py
@@ -3,9 +3,9 @@ import random
 class Genereitor:
     def genereitor_password(letter, number, number_caracter):
         try:
-            o = letter + "," + number
-            f = o.split(',')
-            randon = random.choices(f, k=number_caracter)
+            join_arr = str(letter + "," + number)
+            tratament = join_arr.split(',')
+            randon = random.choices(tratament, k=number_caracter)
             return "".join(randon)
         except Error as e:
             print(e)
